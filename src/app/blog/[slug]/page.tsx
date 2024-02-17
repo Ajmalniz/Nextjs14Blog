67e9e70d0f6082f7a4b42c794aae6a5e75de5ec5
Ajmalniz <128520849+Ajmalniz@users.noreply.github.com>
2024-02-17 16:44:32 +0500

    finshed product

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,47 @@
+
+import { fullBlog } from "@/lib/interface";
+import { client, urlFor } from "@/lib/sanity";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
+
+export const revalidate = 30; // revalidate at most 30 seconds
+
+async function getData(slug: string) {
+  const query = `
+    *[_type == "blog" && slug.current == '${slug}'] {
+        "currentSlug": slug.current,
+          title,
+          content,
+          titleImage
+      }[0]`;
+
+  const data = await client.fetch(query);
+  return data;
+}
+
+export default async function BlogArticle({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data: fullBlog = await getData(params.slug);
+
+  return (
+    <div className="mt-8">
+      
+
+      <Image
+        src={urlFor(data.titleImage).url()}
+        width={800}
+        height={800}
+        alt="Title Image"
+        priority
+        className="rounded-lg mt-8 border"
+      />
+
+      <div className="mt-16 prose prose-blue prose-lg dark:prose-invert prose-li:marker:text-primary prose-a:text-primary">
+        <PortableText value={data.content} />
+      </div>
+    </div>
+  );
+}
---
+++ src/lib/sanity.ts
@@ -0,0 +1,13 @@
+import { createClient } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+export const client = createClient({
+    apiVersion: "2023-05-03",
+    dataset: "production",
+    projectId: "q0plxxu8",
+    useCdn: false,
+  });
+  const builder = imageUrlBuilder(client);
+
+export function urlFor(source: any) {
+  return builder.image(source);
+}
